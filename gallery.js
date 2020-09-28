048789aec7d292ebf0744d61738b5d0f116ce1a0
Elronia <zar.catherine@gmail.com>
2020-09-27 20:31:31 -0400

    adding paintings to the page

--- gallery.js
+++ gallery.js
@@ -1,21 +1,32 @@
-//request to paintings index endpoint
+const paintingsUrl = "http://localhost:3000/paintings"
+//Stable elements
+const pageContainer = document.querySelector("div#page-container")
+console.log(pageContainer)
+// request to paintings index endpoint
 
-// fetch(url)
-// .then(res => res.json())
-// .then(paintingsArr => {
-//     const image = paintingsArr[0].image
-//     const imageElement = document.createElement("img")
-//     imageElement.src = image
-//     imgBox.append(imageElement)
+fetch(paintingsUrl)
+.then(res => res.json())
+.then(paintingsArr => {
+    paintingsArr.forEach( painting => {
+        const image = painting.image
+        const imageDiv = document.createElement("div")
+        const imageElement = document.createElement("img")
+        imageElement.src = image
+        imageElement.style.height = "400px"
+        imageElement.style.width = "325px"
+        const fullSizeButton = document.createElement("button")
+        const favoriteButton = document.createElement("button")
+        imageDiv.append(imageElement, fullSizeButton, favoriteButton)
+        pageContainer.append(imageDiv)
+    })
 
+    // imageElement.addEventListener("click", () =>{
+    //     console.log(paintingsArr)
+    //     //converting html collection to js array
+    //     const htmlArray = Array.from(imgBox.children)
+    //     htmlArray.forEach( child => child.remove())
 
-//     imageElement.addEventListener("click", () =>{
-//         console.log(paintingsArr)
-//         //converting html collection to js array
-//         const htmlArray = Array.from(imgBox.children)
-//         htmlArray.forEach( child => child.remove())
+    //     imgBox.innerText = `${paintingsArr[0].painter.name}\n${paintingsArr[0].painter.nationality}\n${paintingsArr[0].painter.years}`
+    // })
 
-//         imgBox.innerText = `${paintingsArr[0].painter.name}\n${paintingsArr[0].painter.nationality}\n${paintingsArr[0].painter.years}`
-//     })
-
-// })
+})
