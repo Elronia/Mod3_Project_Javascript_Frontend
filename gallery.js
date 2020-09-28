7507701ec5436f8318673fa2779e39dff4de1166
pierrewebdev <patrick.pierre000@gmail.com>
2020-09-27 22:25:47 -0400

    created the gallery page

--- gallery.js
+++ gallery.js
@@ -2,32 +2,66 @@ const paintingsUrl = "http://localhost:3000/paintings"
 //Stable elements
 const pageContainer = document.querySelector("div#page-container")
 console.log(pageContainer)
-// request to paintings index endpoint
 
-fetch(paintingsUrl)
-.then(res => res.json())
-.then(paintingsArr => {
-    paintingsArr.forEach( painting => {
-        const image = painting.image
-        const imageDiv = document.createElement("div")
-        const imageElement = document.createElement("img")
-        imageElement.src = image
-        imageElement.style.height = "400px"
-        imageElement.style.maxWidth = "85%"
 
-        const buttonDiv = document.createElement("div")
-        buttonDiv.className = "btn-side-by-side"
-        const fullSizeButton = document.createElement("button")
-        fullSizeButton.innerText = "see full size"
-        const favoriteButton = document.createElement("button")
-        favoriteButton.innerText = "favorite ♥️"
-        buttonDiv.append(fullSizeButton, favoriteButton)
 
-        imageDiv.append(imageElement,buttonDiv)
-        pageContainer.append(imageDiv)
+
+
+
+
+
+
+//function to be executed on click of Art galery link
+function displayGallery(){
+    fetch(paintingsUrl)
+    .then(res => res.json())
+    .then(paintingsArr => {
+        paintingsArr.forEach( painting => {
+            turnObjectToImage(painting)
+        })
+
     })
 
-    // imageElement.addEventListener("click", () =>{
+}
+//helper function to create an image from a painting object from fetch request
+function turnObjectToImage(paintingObject){
+    const image = paintingObject.image
+    const imageDiv = document.createElement("div")
+
+    //create img tag with styling
+    const imageElement = document.createElement("img")
+    imageElement.src = image
+    imageElement.style.height = "400px"
+    imageElement.style.maxWidth = "85%"
+
+    //create full-size and favorite buttons
+    const buttonDiv = document.createElement("div")
+    buttonDiv.className = "btn-side-by-side"
+    const fullSizeButton = document.createElement("button")
+    fullSizeButton.innerText = "see full size"
+    const favoriteButton = document.createElement("button")
+    favoriteButton.innerText = "favorite ♥️"
+
+    //append unstable elements to pageContainer
+    buttonDiv.append(fullSizeButton, favoriteButton)
+    imageDiv.append(imageElement,buttonDiv)
+    pageContainer.append(imageDiv)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // imageElement.addEventListener("click", () =>{
     //     console.log(paintingsArr)
     //     //converting html collection to js array
     //     const htmlArray = Array.from(imgBox.children)
@@ -35,5 +69,3 @@ fetch(paintingsUrl)
 
     //     imgBox.innerText = `${paintingsArr[0].painter.name}\n${paintingsArr[0].painter.nationality}\n${paintingsArr[0].painter.years}`
     // })
-
-})
